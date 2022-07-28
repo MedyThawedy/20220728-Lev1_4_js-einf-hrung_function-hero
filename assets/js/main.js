20c7d3f915b389a5ed606915259111b26c42b92b
MD <dhmedy@googlemail.com>
2022-07-28 14:53:57 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+function fn_send(hname, hpower, henemy) {
+    console.log('My favorite Marvel hero is: ' + hname);
+    console.log('He/She has the ability: ' + hpower);
+    console.log('His/Her biggest enemy is: ' + henemy);
+
+    document.getElementById('output').innerHTML = ('My favorite Marvel hero is: ' + hname)
+        + '  ' + ('He/She has the ability: ' + hpower)
+        + '  ' + ('His/Her biggest enemy is: ' + henemy);
+}
+
+/* My favorite Marvel hero is:
+He/She has the ability:
+His/Her biggest enemy is:
+ */

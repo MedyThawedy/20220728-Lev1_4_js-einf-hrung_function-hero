2835b9d01abea0b6a1ec415f31657173924a0b9a
MD <dhmedy@googlemail.com>
2022-07-28 15:29:00 +0200

    msg

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,17 @@ function fn_send(hname, hpower, henemy) {
 He/She has the ability:
 His/Her biggest enemy is:
  */
+
+
+
+// Arrow function
+
+let arrow_fn_send = (hname, hpower, henemy) => {
+    console.log('My favorite Marvel hero is: ' + hname);
+    console.log('He/She has the ability: ' + hpower);
+    console.log('His/Her biggest enemy is: ' + henemy);
+
+    document.getElementById('output').innerHTML = ('My favorite Marvel hero is: ' + hname)
+        + '  ' + ('He/She has the ability: ' + hpower)
+        + '  ' + ('His/Her biggest enemy is: ' + henemy);
+}
